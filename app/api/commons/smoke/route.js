31c5598e99b2c320c6273df9d83c71fbe5525e5c
india123445t-pixel <india123445t@gmail.com>
2026-09-18 20:03:13 +0100

    fix: correct temporary Commons smoke import

--- app/api/commons/smoke/route.js
+++ app/api/commons/smoke/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { commonsSubmitAndWait } from '../../../../../lib/aqlevon/commons-edge.js';
+import { commonsSubmitAndWait } from '../../../../lib/aqlevon/commons-edge.js';
 
 export const dynamic='force-dynamic';
 export const maxDuration=60;
